ee085ac8d6132560a7a599b2d7df237a436aa814
Mark-Abdullah <hsabdullah365@gmail.com>
2025-02-16 22:29:15 +0600

    problems 01

---
+++ problems/01.js
@@ -0,0 +1,24 @@
+/*
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the money you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+
+Sample Input:
+1000
+700
+
+Sample Output:
+300
+
+*/
+
+// ------------------------------------------ //
+
+let myMoney = 1000;
+let orangesApples = 700;
+let returnMoney = (myMoney - orangesApples);
+console.log(returnMoney);
