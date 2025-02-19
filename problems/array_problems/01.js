d65b53e2e7bffa5adb4f4fb86bdc1f44ca413145
Mark-Abdullah <hsabdullah365@gmail.com>
2025-02-19 17:48:21 +0600

    Array Problems 01 Added

---
+++ problems/array_problems/01.js
@@ -0,0 +1,17 @@
+/*
+Declare an array
+  1. Declare an array with 5 elements containing fruits
+  2. console log the 3rd index element
+  3. change the value of the 2nd index element to jambura
+  4. console log the final array
+*/
+
+const fruits = ["mango", "apple", "orange", "banana", "papaya"];
+
+
+console.log(fruits[3]);
+
+fruits[2] = "jambura";
+console.log(fruits[2]);
+
+console.log(fruits);
