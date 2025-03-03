b52c0d27d6e307eb223be25cbdb6983a03575304
Mark-Abdullah <hsabdullah365@gmail.com>
2025-03-03 19:33:12 +0600

    Real Time Added

--- Practice-Project/task-board/script.js
+++ Practice-Project/task-board/script.js
@@ -1,6 +1,7 @@
 // Variable Store\
 let randomColor = document.getElementById('random-bg');
 let mainBody = document.getElementById('main_body');
+let realTimeDate = document.getElementById('real_time_date');
 
 // Random Color
 function randomColorCreate(){
@@ -12,3 +13,19 @@ function randomColorCreate(){
 randomColor.addEventListener('click', () => {
   mainBody.style.backgroundColor = randomColorCreate();
 })
+// Discover
+document.getElementById('discoverNew').addEventListener('click', () => {
+  window.location.href = "./task-board/blog";
+})
+// Real Time Date
+function todayDate(){
+  let day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let d = new Date();
+  let dayName = day[d.getDay()];
+  let monthName = month[d.getMonth()];
+  let date = d.getDate();
+  let year = d.getFullYear();
+  return `${dayName}, <br> <p class="text-[18px]; font-semibold">${monthName} ${date} ${year}</p>`;
+}
+realTimeDate.innerHTML = todayDate();
